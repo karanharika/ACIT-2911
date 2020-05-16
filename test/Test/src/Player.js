dc45da99be6a9bdde5e270a07aea41e2cb52017c
Andrew Jayasinghe <heshronferdi@gmail.com>
2020-05-15 21:51:36 -0700

    Update Player.js

--- test/Test/src/Player.js
+++ test/Test/src/Player.js
@@ -3,16 +3,16 @@
 
 function Player() {
 }
-Player.play = function(song) {
+Player.prototype.play = function(song) {
   this.currentlyPlayingSong = song;
   this.isPlaying = true;
 };
 
-Player.pause = function() {
+Player.prototype.pause = function() {
   this.isPlaying = false;
 };
 
-Player.resume = function() {
+Player.prototype.resume = function() {
   if (this.isPlaying) {
     throw new Error("song is already playing");
   }
@@ -21,7 +21,7 @@ Player.resume = function() {
 };
 
 
-Player.skip = function() {
+Player.prototype.skip = function() {
   if (this.isSkipped) {
     throw new Error("song is not skipped");
   }
@@ -29,7 +29,7 @@ Player.skip = function() {
   this.isSkipped = true;
 };
 
-Player.stop = function() {
+Player.prototype.stop = function() {
   if (this.isStopped) {
     throw new Error("song is not skipped");
   }
