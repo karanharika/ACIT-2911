a8ab91e401d02ca823e3b70b23beedec22fb6cff
karanharika <55457934+karanharika@users.noreply.github.com>
2020-05-20 14:12:23 -0700

    Updated pfp file
    changed target folder

--- My Music/upload.php
+++ My Music/upload.php
@@ -1,7 +1,7 @@
 <?php
 if (($_FILES['my_file']['name']!="")){
 // Where the file is going to be stored
-	$target_dir = "audio/";
+	$target_dir = "upload/";
 	$file = $_FILES['my_file']['name'];
 	$path = pathinfo($file);
 	$filename = $path['filename'];
